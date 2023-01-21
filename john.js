5a6fe0e3e0d958a2076ed16a007261063502b4b1
Vikramv2789 <99269958+Vikramv2789@users.noreply.github.com>
2023-01-21 09:51:55 -0500

    Add files via upload

--- john.js
+++ john.js
@@ -2,7 +2,7 @@ function force() {
     standard = document.getElementById("standard");
     Item = document.getElementById("foodsel");
     countBox = document.getElementById("count");
-    ShoppingList = ["Oranges", "Juice", "Bacon"];
+    ShoppingList = ["Oranges", "Juice", "Bacon", "Skittles"];
     showcase();
 }
 
@@ -13,13 +13,13 @@ function showcase() {
     }
 }
 
-function indexesOf(array, itm) {
-    let idxList = [];
+function indxsOf(array, items) {
+    let idexList = [];
 
     for (let f = 0; f < array.length; f++)
-        if (itm === array[f])
-            idxList.push(f);
-    return idxList;
+        if (items === array[f])
+            idexList.push(f);
+    return idexList;
 }
 
 function Addanitem() {
@@ -32,22 +32,22 @@ function Addanitem() {
 
 function DeleteanItem() {
     if(ShoppingList.includes(Item.value)){
-        ShoppingList.splice(indexesOf(ShoppingList, Item.value), 1);
+        ShoppingList.splice(indxsOf(ShoppingList, Item.value), 1);
         showcase();
     }
 }
 
-function MoveUpandMoveDown(new_indexP) {
-    let old_index = parseInt(Item.value);
-    let new_index = old_index + parseInt(new_indexP);
-    if(new_index < 0){
+function MoveUpandMoveDown(frontal_indexP) {
+    let past_index = parseInt(Item.value);
+    let frontal_index = past_index + parseInt(frontal_indexP);
+    if(frontal_index < 0){
         return;
     }
-    if(new_index > ShoppingList.length-1){
-        new_index--;
+    if(frontal_index > ShoppingList.length-1){
+        frontal_index--;
         return;
     }
-    ShoppingList.splice(new_index, 0, ShoppingList.splice(old_index, 1)[0]);
-    Item.value = new_index;
+    ShoppingList.splice(frontal_index, 0, ShoppingList.splice(past_index, 1)[0]);
+    Item.value = frontal_index;
     showcase();
 };
